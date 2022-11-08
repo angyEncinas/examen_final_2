2be216ff560a6f7db9fd1457bac17be05eeba4c4
Alfred <alfred.garcia@ucb.edu.bo>
2022-11-08 09:19:36 -0400

    adicion de tarea - basico

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,25 @@
-import sumar from "./sumador";
+//import sumar from "./sumador";
 
-const first = document.querySelector("#primer-numero");
-const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
-const div = document.querySelector("#resultado-div");
+const materias = document.querySelector("#materias-items");
+const tarea = document.querySelector("#tarea-item");
+const fecha = document.querySelector("#fecha-item");
 
-form.addEventListener("submit", (event) => {
+const crear = document.querySelector("#crear-form");
+
+const vista = document.querySelector("#vista-div");
+
+const mensaje = "Tarea creada con exito !!!";
+
+crear.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
+  const MateriasList = materias.value;
+  const Tarea_Text = tarea.value;
+  const Fecha_Text = fecha.value;
 
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+  vista.innerHTML = "<p>" + mensaje + "<p>" +
+                    "<p> Materia:" + MateriasList + "<p>" +
+                    "<p> Tarea:" + Tarea_Text + "<p>" +
+                    "<p> Fecha:"+ Fecha_Text + "<p>" +
+                    "</p>";
 });
