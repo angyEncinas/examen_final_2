23e4929a705e16d26f1047e9ed51533b7f5f28d2
Alfred <alfred.garcia@ucb.edu.bo>
2022-11-09 12:45:19 -0400

    Creacion de Tareas

--- src/docente.js
+++ src/docente.js
@@ -1,1 +1,12 @@
-//
+function crearTarea(nombreMateria,nombreTarea,fechaInicio,fechaFin)
+{
+    let tarea = {
+        nombreMateria: nombreMateria,
+        nombreTarea: nombreTarea,
+        fechaInicio: fechaInicio,
+        fechaFin: fechaFin
+    }
+    return tarea
+}
+
+export {crearTarea}
--- src/docente.spec.js
+++ src/docente.spec.js
@@ -1,1 +1,12 @@
-//
+import {crearTarea} from "./docente.js";
+
+describe("TAREA", () => {
+   it("Se crea una tarea", () => {
+       expect(crearTarea("Calculo I", "Ecuaciones cubicas", "09/11/2022", "10/11/2022")).toEqual({
+            nombreMateria: "Calculo I",
+            nombreTarea: "Ecuaciones cubicas",
+            fechaInicio: "09/11/2022",
+            fechaFin: "10/11/2022",
+       });
+   });
+});
