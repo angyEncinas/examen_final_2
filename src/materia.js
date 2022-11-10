96ddb4fd93c5d6953afee66b4f1cf1932390918a
Angy Encinas <angy.encinas@ucb.edu.bo>
2022-11-09 22:51:57 -0400

    materia con docente(CREAR COMPLETADO)

--- src/materia.js
+++ src/materia.js
@@ -2,16 +2,21 @@ class Materia {
     constructor() {
         this.nombreMateria = "";
         this.descripcion = "";
+        this.docente = "";
     }
-    crearMateria(siglaynombre,descripcion){
+    crearMateria(siglaynombre,descripcion,docente){
         this.nombreMateria = siglaynombre;
         this.descripcion = descripcion;
+        this.docente = docente;
     }
     obtenerNombre(){
         return this.nombreMateria;
     }
     obtenerDescripcion(){
         return this.descripcion;
+    }
+    obtenerDocente(){
+        return this.docente;
     }
 }
 
--- src/materia.test.js
+++ src/materia.test.js
@@ -3,12 +3,16 @@ import Materia from "./materia.js";
 describe("CREAR MATERIA", () => {
     let materia = new Materia();
     it("Deberia devolver la sigla de la materia y el nombre", () => {
-        materia.crearMateria("LEN-5 Ingles V","Aprende ingles");
+        materia.crearMateria("LEN-5 Ingles V","Aprende ingles", "Susan");
         expect(materia.obtenerNombre()).toEqual("LEN-5 Ingles V");
     });
     
     it("Deberia devolver una corta descripcion de la materia", () => {
-        materia.crearMateria("LEN-5 Ingles V","Aprende ingles");
+        materia.crearMateria("LEN-5 Ingles V","Aprende ingles", "Susan");
         expect(materia.obtenerDescripcion()).toEqual("Aprende ingles");
+    });
+    it("Deberia devolver el docente encargado de la materia", () => {
+        materia.crearMateria("LEN-5 Ingles V","Aprende ingles", "Susan");
+        expect(materia.obtenerDocente()).toEqual("Susan");
     });
 });
