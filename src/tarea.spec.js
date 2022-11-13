f63ec05fa3947960fde62b30b8f47288cd75a89a
Alfred <alfred.garcia@ucb.edu.bo>
2022-11-13 17:38:29 -0400

    Correccion de pruebas unitarias

--- src/tarea.spec.js
+++ src/tarea.spec.js
@@ -5,7 +5,7 @@ import Tareas from "./tareas.js"
 describe("Crear una tarea", () => {
     let tareas = new Tarea;
     it("Creara una tarea con detalles simples", () => {
-        expect(tareas.crear('diagramas', 'detalles', 'sis info', '05/2022')).toEqual("tareaCreada")
+        expect(tareas.crear('diagramas', 'detalles', 'sis info', '05/2022')).toEqual("Creada con exito!.")
     });
 
     it("Mostrar una tarea con detalles simples", () => {
@@ -19,24 +19,21 @@ describe("Guardar tareas", () => {
     let listaTareas = new Tareas();
     let tarea = new Tarea();
     it("Crear una tarea", () => {
-        expect(tarea.crear('diagramas', 'detalles', 'sis info', '05/2022')).toEqual("tareaCreada");
+        expect(tarea.crear('diagramas', 'detalles', 'sis info', '05/2022')).toEqual("Creada con exito!.");
     });
 
     it("Guardar una tarea", () => {
-        expect(listaTareas.guardar(tarea)).toEqual("tareaGuardada");
+        expect(listaTareas.guardar(tarea)).toEqual("Tarea Guardada");
     });
 
     it("Sobreescribir una tarea", () => {
-        expect(tarea.crear('diagramas2', 'detalles2', 'sis info2', '05/20222')).toEqual("tareaCreada");
+        expect(tarea.crear('diagramas2', 'detalles2', 'sis info2', '05/20222')).toEqual("Creada con exito!.");
     });
 
     it("Guardar otra tarea", () => {
-        expect(listaTareas.guardar(tarea)).toEqual("tareaGuardada");
+        expect(listaTareas.guardar(tarea)).toEqual("Tarea Guardada");
     });
-
 });
-
-
 
 /*describe("Guardar una tarea", () => {
     it("Creara una tarea con detalles simples", () => {
