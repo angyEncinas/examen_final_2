b669c6f7c394f397e50033014f91e59d9fc51ac4
Alfred <alfred.garcia@ucb.edu.bo>
2022-11-16 16:10:05 -0400

    Adicion de pruebas unitarias para tarea

--- src/tarea.spec.js
+++ src/tarea.spec.js
@@ -1,30 +1,43 @@
 import Tarea from "./tarea.js"
+let tareas = new Tarea;
 
+describe("CREAR UNA TAREA", () => {
+    it("Deberia devolver el titulo de la tarea", () => {
+        materia.crearTarea("Proyectos de grados");
+        expect(materia.mostrar()).toEqual("Proyectos de grados");
+    });
 
-describe("Crear una tarea", () => {
-    let tareas = new Tarea;
-    it("Creara una tarea con detalles simples", () => {
+    it("Deberia devolver la descripcion de la tarea", () => {
+        materia.crearTarea("Investigar metodologias agiles");
+        expect(materia.mostrar()).toEqual("Investigar metodologias agiles");
+    });
+
+    it("Deberia devolver el nombre de la materia de la tarea", () => {
+        materia.crearTarea("Taller de desarrollo");
+        expect(materia.mostrar()).toEqual("Taller de desarrollo");
+    });
+
+    it("Deberia devolver la fecha de la tarea", () => {
+        materia.crearTarea("16/11/2022");
+        expect(materia.mostrar()).toEqual("16/11/2022");
+    });
+
+    it("Crear una tarea con detalles simples", () => {
         expect(tareas.crear('diagramas', 'detalles', 'sis info', '05/2022')).toEqual("Creada con exito!.")
     });
 
     it("Mostrar una tarea con detalles simples", () => {
         expect(tareas.mostrar()).toEqual("titulo:diagramas, descripcion:detalles, materia:sis info, fecha:05/2022")
     });
-
-
 });
 
-describe("Guardar tareas", () => {
+describe("GUARDAR TAREAS", () => {
     let tarea = new Tarea();
     it("Crear una tarea", () => {
         expect(tarea.crear('diagramas', 'detalles', 'sis info', '05/2022')).toEqual("Creada con exito!.");
     });
 
-
     it("Sobreescribir una tarea", () => {
         expect(tarea.crear('diagramas2', 'detalles2', 'sis info2', '05/20222')).toEqual("Creada con exito!.");
     });
-
-
 });
-
